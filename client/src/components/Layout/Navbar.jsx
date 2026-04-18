a09ce316b599ee131ff5797411c78d8c6f47817b
ALPHA <saikatmahara7895@gmail.com>
2026-04-18 22:43:26 +0530

    Navbar Features ADD

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import { useState, useEffect } from "react";
+import "./navbar.css"
 
 function Navbar() {
     const { cartCount } = useCart();
